37233dbf699fd298eb13f15c81028c77b82574cf
Anton Kim <aniashevtsova@gmail.com>
2022-12-12 18:38:50 -0800

    Dec 12

--- js/main.js
+++ js/main.js
@@ -23,3 +23,23 @@ navMenu.classList.remove('show')
 navLink.forEach (n=> n.addEventListener('click', linkAction))
 
 /*===== SCROLL SECTIONS ACTIVE LINK =====*/
+const sections = document.querySelectorAll('section[id]')
+
+window.addEventListener('scroll', scrollActive)
+
+function scrollActive(){
+    const scrollY = window.pageYOffset
+
+    sections.forEach(current =>{
+        const sectionHeight = current.offsetHeight
+        const sectionTop = current.offsetTop - 50
+        const sectionId = current.getAttribute('id')
+    })
+
+    if(scrollY > sectionTop && scrollY <= sectionTop + sectionHeight){
+        document.querySelector('nav_menu a[href* = ' + sectionId + ']').classList.add('active')
+    } else {
+        document.querySelector('nav_menu a[href* = ' + sectionId + ']').classList.remove('active')
+
+}
+}
